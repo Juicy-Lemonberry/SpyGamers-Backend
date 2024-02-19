db90abebf95bac614e08cacdb61298dd58dcceaa
WQMan <wqyeo@outlook.com>
2024-02-19 22:22:29 +0800

    Route to search for users

--- SpyGamersApi/src/routers/accounts.ts
+++ SpyGamersApi/src/routers/accounts.ts
@@ -1,11 +1,13 @@
 import { FastifyInstance } from 'fastify';
 import * as registerController from '../controllers/accounts/register';
 import * as loginController  from '../controllers/accounts/login';
+import * as searchUserController from '../controllers/accounts/searchUser';
 
 
 export default function (fastify: FastifyInstance, opts: any, done: Function) {
   fastify.post('/register', { schema: { body: registerController.registerSchema } }, registerController.register);
   fastify.post('/login', { schema: { body: loginController.loginSchema } }, loginController.login);
+  fastify.get('/search-user', { schema: { querystring: searchUserController.searchUserSchema } }, searchUserController.searchUser);
 
   done();
 }
