34c7e2adf001fd799d726867bb3919385815a283
WQMan <wqyeo@outlook.com>
2024-02-23 22:20:29 +0800

    Route to change username

---
+++ SpyGamersApi/src/controllers/accounts/changeUsername.ts
@@ -0,0 +1,51 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { PrismaClient } from '@prisma/client';
+import { tryFindAccountBySessionToken } from '../../utils/tryFindAccountBySessionToken';
+
+const prisma = new PrismaClient();
+
+export const changeUsername = async (request: FastifyRequest, reply: FastifyReply) => {
+    const { auth_token, new_username } = request.body as { auth_token: string; new_username: string; };
+    try {
+        const account = await tryFindAccountBySessionToken(auth_token, prisma);
+        if (!account) {
+            return reply.status(401).send({ status: "FAILURE" });
+        }
+
+        const existingUser = await prisma.account.findFirst({
+            where: {
+                username: new_username,
+            },
+        });
+
+        if (existingUser != null) {
+            return reply.status(406).send({ status: "USERNAME_TAKEN" });
+        }
+
+        await prisma.account.update({
+            data: {
+                username: new_username
+            },
+            where: {
+                id: account.id
+            }
+        })
+
+
+
+        return reply.status(201).send({ status: "SUCCESS" });
+    } catch (error) {
+        console.error("Error:", error);
+        return reply.status(500).send({ status: "FAILURE" });
+    }
+};
+
+
+export const changeUsernameSchema = {
+    type: 'object',
+    required: ['auth_token', 'new_username'],
+    properties: {
+        auth_token: { type: 'string' },
+        new_username: { type: 'string' }
+    },
+};
--- SpyGamersApi/src/routers/accounts.ts
+++ SpyGamersApi/src/routers/accounts.ts
@@ -3,12 +3,15 @@ import * as registerController from '../controllers/accounts/register';
 import * as loginController  from '../controllers/accounts/login';
 import * as searchUserController from '../controllers/accounts/searchUser';
 import * as changeProfilePictureController from '../controllers/accounts/changeProfilePicture';
+import * as changeUsernameController from '../controllers/accounts/changeUsername';
 
 export default function (fastify: FastifyInstance, opts: any, done: Function) {
   fastify.post('/register', { schema: { body: registerController.registerSchema } }, registerController.register);
   fastify.post('/login', { schema: { body: loginController.loginSchema } }, loginController.login);
   fastify.get('/search-user', { schema: { querystring: searchUserController.searchUserSchema } }, searchUserController.searchUser);
+  
   fastify.put('/change-profile-picture', { schema: { body: changeProfilePictureController.changeProfilePictureSchema }}, changeProfilePictureController.changeProfilePicture);
+  fastify.put('/change-username', { schema: { body: changeUsernameController.changeUsernameSchema } }, changeUsernameController.changeUsername);
 
   done();
 }
