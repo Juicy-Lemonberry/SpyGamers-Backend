eac895fc6fe54aac17aca3e707e70f919f39f86d
WQMan <wqyeo@outlook.com>
2024-03-08 00:30:54 +0800

    Updated to use chat image of tinyllama instead...

--- ollama-run-tinyllama.sh
+++ ollama-run-tinyllama.sh
@@ -2,4 +2,4 @@
 
 # Run this script after ollama container is up.
 # NOTE: You can create a new screen before running this script, allowing you to close the terminal, but still letting this script run...
-docker exec -it ollama ollama run tinyllama
+docker exec -it ollama ollama run tinyllama:chat
