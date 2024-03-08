344a9e033be7a01ea4c13fd2f32ca4f1f96f3e2b
WQMan <wqyeo@outlook.com>
2024-03-09 01:12:38 +0800

    Routes to get all members in a group

---
+++ SpyGamersApi/src/controllers/groups/get-group-members.ts
@@ -0,0 +1,67 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { PrismaClient, Prisma } from '@prisma/client';
+import { tryFindAccountBySessionToken } from '../../utils/tryFindAccountBySessionToken';
+
+const prisma = new PrismaClient();
+
+export const getGroupMembers = async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+        const { auth_token, group_id } = request.body as { auth_token: string; group_id: number; };
+
+        const account = await tryFindAccountBySessionToken(auth_token, prisma);
+        if (!account) {
+            return reply.status(401).send({ status: "BAD_AUTH" });
+        }
+
+        // Check if the account is a member of the specified group
+        const isMember = await prisma.groupMember.findFirst({
+            where: {
+                account_id: account.id,
+                group_id: group_id
+            }
+        });
+
+        if (!isMember) {
+            return reply.status(406).send({ status: "NOT_GROUP_MEMBER" });
+        }
+
+        // Fetch all accounts that are members of the specified group
+        const groupMembers = await prisma.groupMember.findMany({
+            where: {
+                group_id: group_id
+            },
+            include: {
+                account: {
+                    select: {
+                        id: true,
+                        username: true,
+                        created_at: true
+                    }
+                }
+            }
+        });
+
+        // Format the response data
+        const data = groupMembers.map(member => ({
+            id: member.account.id,
+            username: member.account.username,
+            is_admin: member.is_admin,
+            account_creation_date: member.account.created_at
+        }));
+
+        reply.status(200).send({ status: "SUCCESS", result: data });
+    } catch (error) {
+        console.error("Error:", error);
+        reply.status(500).send({ status: "FAILURE" });
+    }
+};
+
+
+export const getGroupMembersSchema = {
+    type: 'object',
+    required: ['auth_token', 'group_id'],
+    properties: {
+        auth_token: { type: 'string' },
+        group_id: { type: 'number' }
+    },
+};
--- SpyGamersApi/src/routers/groups.ts
+++ SpyGamersApi/src/routers/groups.ts
@@ -3,12 +3,14 @@ import { FastifyInstance } from 'fastify';
 import { createGroup, createGroupSchema } from '../controllers/groups/create-group';
 import { getAccountGroups, getAccountGroupsSchema } from '../controllers/groups/get-account-groups';
 import { getGroupMessages, getGroupMessagesSchema } from '../controllers/groups/get-group-messages';
+import { getGroupMembers, getGroupMembersSchema } from '../controllers/groups/get-group-members';
 
 export default function (fastify: FastifyInstance, opts: any, done: Function) {
 
   fastify.post('/create-group', { schema: { body: createGroupSchema } }, createGroup);
   fastify.post('/get-account-groups', { schema: { body: getAccountGroupsSchema}}, getAccountGroups);
   fastify.post('/get-group-messages', { schema: { body: getGroupMessagesSchema}}, getGroupMessages);
+  fastify.post('/get-group-members', {schema: { body: getGroupMembersSchema}}, getGroupMembers)
 
   done();
 }
